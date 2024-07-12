aa7aff1ed41dbc8902d59c52a43093a22c78387d
dofor <ljh730673@gmail.com>
2024-07-12 17:09:07 +0900

    java

---
+++ java-alone1/Taiyaki/src/tset/반복문.java
@@ -0,0 +1,22 @@
+package tset;
+
+public class 반복문 {
+	public static void main(String[] args) {
+		
+		int 표현식 = 1; // 표현식에 사용할 변수 값 설정
+	
+	    switch (표현식) {
+            case 1:
+                // 표현식이 값1과 일치할 때 실행될 코드
+                break;
+            case 2:
+                // 표현식이 값2와 일치할 때 실행될 코드
+                break;
+            // 여러 개의 case 문을 추가할 수 있습니다.
+            default:
+                // 표현식이 어떤 case 값과도 일치하지 않을 때 실행될 코드
+                break;
+        }
+    }
+}
+
