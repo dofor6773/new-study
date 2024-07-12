3cd326bd2bafdee4e6b8b204e11f6013fe008d9c
dofor <ljh730673@gmail.com>
2024-07-12 11:02:47 +0900

    html-css-javascript

---
+++ html-css-javascript/dey7/js/main.js
@@ -0,0 +1,11 @@
+document.write('body mainjs파일');
+
+// String(문자 데이터)
+// 따옴표를 사용합니다.
+let myName = "HEROPY";
+let email = 'thesecon@gmail.com';
+let hello = `Hello`
+console.log(myName); // HEROPY
+console.log(email); // thesecon@gmail.com
+console.log(hello); // Hello HEROPY?!
+
